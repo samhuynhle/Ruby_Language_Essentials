9da36b6b1e02866b2e9aa7096022b35da52f9631
samhuynhle <samhuynhle@gmail.com>
2019-10-19 20:36:35 -0700

    reviewed numbers in Ruby

---
+++ numbers.rb
@@ -0,0 +1,33 @@
+# puts 1
+# puts 5.4
+# puts -3
+# puts 5 + 8
+# puts 8 - 3
+# puts 9 * 9
+# puts 9 / 3
+# puts 2**3 # two to the third
+# puts 10 % 3 # modulus
+# num = 20
+# puts num
+# puts ("my favorite num " + num.to_s) # this is how you print string and num together
+
+# Methods
+# puts num.abs() # absolute value of num
+
+# num  = 20.48
+# puts num.round() # rounds to closer edge
+# puts num.ceil() # Round up always
+# puts num.floor() # Round down always
+
+# There's the Math Class as well we can use to perform operations
+
+# puts Math.sqrt(num)
+# puts Math.sqrt(36)
+# puts Math.log(2)
+
+# In ruby we have int and float
+
+puts 1 + 7 # returns an int
+puts 1.0 + 7 # returns a float
+puts 10 / 7 # returns int
+puts 10 / 7.0 # returns float
