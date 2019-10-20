10f3218a5733758c0f1996f80d0da01ffd366317
samhuynhle <samhuynhle@gmail.com>
2019-10-19 20:29:25 -0700

    First init for Ruby Language Essentials refresher

---
+++ one.rb
@@ -0,0 +1,37 @@
+# puts "   /|"
+# puts "  / |"
+# puts " /  |"
+# puts "/___|"
+
+# Ruby is looking through each line individually.
+
+# Datatypes
+# name = "Mike" # string
+# age = 75 # int
+# gpa = 3.2 # float
+# isLarge = false # boolean
+# flaws = nil # nil, has no value
+
+# phrase = "You're \"so\" Cool"
+# puts phrase
+
+# String Methods
+ phrase = "You're so cool"
+#  puts phrase.upcase() # converts to uppcase
+
+#  puts phrase.downcase()
+
+#  puts phrase.strip() # get's rid of leading and trailing white space
+
+#  puts phrase.length() # includes spaces in the string
+
+#  puts phrase.include? "cool"
+#  puts phrase.include? "Hello" # the inlcude? return a boolean
+
+# puts phrase[0] # indexing through a string.
+# Starting at 0, just like a list.
+
+# Can also print out a range
+# puts phrase[0,3] # start the range at index 0 and go to three, but not include 3
+
+# puts phrase.index("Y") # identifies the index that is the character or where the word starts You're returns 0
